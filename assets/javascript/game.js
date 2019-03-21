bca18c8f6e00381b41ebaa1fcca58080cec55813
KevinL <mastahkevin@gmail.com>
2019-03-21 01:02:14 -0700

    Added crystals to use 4 instead of 3.Changed pseudocode so I can focus better on the task. Added a bit of structure to JS as well.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,62 @@
 /*
-1. There will be four crystals displayed as buttons on the page.
-    a. Each crystal has a random number assigned.
-
-2. The player will be shown a random number at the start of the game.
-    a. Target
-
-3. When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-    a. On click for each individual ID
-
-4. Your game will hide this amount until the player clicks a crystal.
-    a. When they do click one, update the player's score counter.
-    b. The player wins if their total score matches the random number from the beginning of the game.
-
-5. The player loses if their score goes above the random number.
-    a. Decrement score
-
-6. The game restarts whenever the player wins or loses.
-    a. When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-7. The app should show the number of games the player wins and loses.
-    a. Button to reset game
+TODO: - Goal of game is to click each of the 4 crytals in order to hit the Magic Number
+TODO: - Single number give randomly (aka Magic Number)
+TODO: - Every crystal needs to have a randomly generated number
+TODO: - Each time a crystal is clicked we will add previous score to new score until number is either the same or goes over magic number.
+TODO: - If number goes over magic number, we lose
+    - decrement loss
+TODO: - If number matches magic number, we win
+    - increment wins
+TODO: - Random number is generated each time player clicks new game button
+    - start over
 */
 
-$(document).ready(function() {
-    var magicNum;
-    var totalScore = 0;
+$(document).ready(function () {
+    // delcare variables
+    var score = 0;
     var wins = 0;
     var losses = 0;
-    var rubyNum;
-    var diamondNum;
-    var emeraldNum;
+    var blackCrystal = Math.floor(Math.random() * 10 + 1);
+    var purpleCrystal = Math.floor(Math.random() * 10 + 1);
+    var greenCrystal = Math.floor(Math.random() * 10 + 1);
+    var redCrystal = Math.floor(Math.random() * 10 + 1);
 
-    function newNums() {
-        magicNum = Math.floor(Math.random() * 125) + 50;
-        rubyNum = Math.floor(Math.random() * 20) * 10;
-        diamondNum = Math.floor(Math.random() * 20) * 10;
-        emeraldNum = Math.floor(Math.random() * 20) * 10;
+    // Set magic number and display
+    var magicNum = Math.floor(Math.random() * 10 + 1);
+
+
+    // function to set new game
+	function newGame() {
+		newNums();
+		totalScore = 0;
+		$("#magicNumber").text(magicNum);
+		$("#totalScore").text(score);
+		$("#blackCrystal").attr("crystalvalue", blackCrystal);
+		$("#purpleCrystal").attr("crystalvalue", purpleCrystal);
+		$("#greenCrystal").attr("crystalvalue", greenCrystal);
+		$("#redCrystal").attr("crystalvalue", redCrystal);
+		$("#wins").text(wins);
+		$("#losses").text(losses);
+		$("#winOrLose").text("");
     }
 
-});
+    // function for winning
+    function youWon() {
+        if (score === magicNum){
+            win+=1;
 
+        }
+    }
+
+
+    // function for losing
+
+    
+    // call new game again to reset game board
+ 
+
+    // add values for score
+
+
+    // conditionals for verifying 
+});
